ee18e686181b25d79a5babf4c823004089c364e8
Hussein Termos <htermos4@gmail.com>
2025-11-07 21:36:09 +0200

    Update diff.c

--- diff.c
+++ diff.c
@@ -1,4 +1,3 @@
-//A Simple C program
 #include "types.h"
 #include "stat.h"
 #include "fcntl.h"
@@ -6,7 +5,6 @@
 
 #define MAX_LINE_LEN 10
 #define MAX_LINES 10
-// o1 = dp[i-1][j] o2 = dp[i][j-1] o3 = dp[i-1][j-1]
   
 int main(int argc, char *argv[])
 {
@@ -16,8 +14,12 @@ int main(int argc, char *argv[])
   }
   int f1 = open(argv[1], O_RDONLY);
   int f2 = open(argv[2], O_RDONLY);
-  if (f1 < 0 || f2 < 0) {
+  if (f1 < 0) {
     printf(2, "Error: could not open file\n");
+  }
+  if (f2 < 0) {
+    printf(2, "Error: could not open file 2\n");
+    exit();
   }
   char f1_data[MAX_LINES][MAX_LINE_LEN];
   char f2_data[MAX_LINES][MAX_LINE_LEN];
@@ -26,11 +28,6 @@ int main(int argc, char *argv[])
   while (f2_len < MAX_LINES && gets_fd(f2, f2_data[f2_len], MAX_LINE_LEN)) f2_len++;
   
   close(f1); close(f2);
-  if (f1_len == 0 || f2_len == 0) {
-    // print all of other file
-    printf(1, "OK");
-    exit();
-  }
   int dp[MAX_LINES][MAX_LINES];
   dp[0][0] = !strcmp(f1_data[0], f2_data[0]) ? 1 : 0;
   for (int i = 1; i < f1_len; ++i) {
@@ -39,70 +36,55 @@ int main(int argc, char *argv[])
   for (int i = 1; i < f2_len; ++i) {
     dp[0][i] = (dp[0][i-1] || !strcmp(f1_data[0], f2_data[i])) ? 1 : 0;
   }
-  // int decisions[MAX_LINES][MAX_LINES];
   for (int i = 1; i < f1_len; ++i) {
     for (int j = 1; j < f2_len; ++j) {
       if (dp[i-1][j] > dp[i][j-1]) {
         dp[i][j] = dp[i-1][j];
-        // decisions[i][j] = 0;
       }
       else {
         dp[i][j] = dp[i][j-1];
-        // decisions[i][j] = 1;
       }
       if (!strcmp(f1_data[i], f2_data[j])) {
-        if (dp[i-1][j-1] + 1 > dp[i][j]) {
-          dp[i][j] = 1 + dp[i-1][j-1];
-          // decisions[i][j] = 2;
-        }
+        dp[i][j] = 1 + dp[i-1][j-1];
       }
     }
   }
-  int curi = 0, curj = 0;
+  int curi = f1_len - 1, curj = f2_len - 1;
+  int diag[MAX_LINES+2][2];
+  int diag_len = dp[f1_len - 1][f2_len - 1] + 2;
+  int curd = diag_len - 2;
+  diag[diag_len - 1][0] = f1_len;
+  diag[diag_len - 1][1] = f2_len;
 
-  while (1) {
-    if (curi == f1_len - 1 && curj == f2_len - 1)
-      break;
-    int best = dp[curi][curj];
-    int nexti = curi, nextj = curj;
-    if (curi + 1 < f1_len && dp[curi + 1][curj] >= best) {
-      best = dp[curi + 1][curj];
-      nexti = curi + 1;
-      nextj = curj;
-    }
 
-    if (curj + 1 < f2_len && dp[curi][curj + 1] >= best) {
-      best = dp[curi][curj + 1];
-      nexti = curi;
-      nextj = curj + 1;
-    }
+  while (curi - 1 >= 0 && curj - 1 >= 0) {
 
-    if (curi + 1 < f1_len && curj + 1 < f2_len && dp[curi + 1][curj + 1] >= best) {
-      best = dp[curi + 1][curj + 1];
-      nexti = curi + 1;
-      nextj = curj + 1;
+    if (dp[curi - 1][curj] == dp[curi][curj]) {
+      curi--;
     }
-    
-    if (nexti == curi && nextj == curj)
-      break;
-
-    if (best == dp[curi + 1][curj]) {
-      printf(1, "- %s\n", f1_data[curi]);
-    }
-    else if (best == dp[curi][curj+ 1]) {
-      printf(1, "+ %s\n", f2_data[curj]);
-    }
+    else if (dp[curi][curj - 1] == dp[curi][curj]) curj--;
     else {
-      printf(1, "%s\n", f1_data[curi]);
+      diag[curd][0] = curi--;
+      diag[curd--][1] = curj--; // proven
     }
+  }
+  diag[curd][0] = curi;
+  diag[curd][1] = curj;
+  while (curi >= 0 && dp[curi][0] != 0) diag[curd][0] = curi--;
+  while (curj >= 0 && dp[0][curj] != 0) diag[curd][1] = curj--;
 
+  diag[0][0] = -1;
+  diag[0][1] = -1;
 
-    curi = nexti;
-    curj = nextj;
+  for (int i = 0; i < diag_len - 1; i++) {
+    for (int j = diag[i][0] + 1; j < diag[i+1][0]; j++) {
+      printf(1, "- %s", f1_data[j]);
+    }
+    for (int j = diag[i][1] + 1; j < diag[i+1][1]; j++) {
+      printf(1, "+ %s", f2_data[j]);
+    }
   }
 
-
-  printf(1, "LCS: %d\n", dp[f1_len - 1][f2_len - 1]);
   exit();
 
 }
